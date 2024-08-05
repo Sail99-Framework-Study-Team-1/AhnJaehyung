8a2ba13068772af4f20a1c720342c8d1b617151e
Jay Ahn <jay@xerapix.studio>
2024-08-05 16:22:39 +0900

    feat: Adding password hash on BulletinBoard

--- src/main/java/com/example/demo/service/BulletinBoardService.java
+++ src/main/java/com/example/demo/service/BulletinBoardService.java
@@ -4,14 +4,19 @@ import com.example.demo.domain.BulletinBoard;
 import com.example.demo.repository.BulletinBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class BulletinBoardService {
     @Autowired BulletinBoardRepository bulletinBoardRepository;
+
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public List<BulletinBoard> getAllBulletinBoard(){
         return bulletinBoardRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
@@ -22,6 +27,9 @@ public class BulletinBoardService {
     }
 
     public BulletinBoard postBulletinBoard(BulletinBoard bulletinBoard) {
+        String password = bulletinBoard.getPassword();
+        String hashedPassword = passwordEncoder.encode(password);
+        bulletinBoard.setPassword(hashedPassword);
         return bulletinBoardRepository.save(bulletinBoard);
     }
 }
